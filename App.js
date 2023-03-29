413ab0a7d13dfd1ff688ddf3f62a4c8ace44dfef
Noura almusaynid <70031377+Noura100k@users.noreply.github.com>
2023-03-29 16:49:55 +0300

    u

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Classifer from './classifier/index';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <Classifer/>
     </View>
   );
 }
